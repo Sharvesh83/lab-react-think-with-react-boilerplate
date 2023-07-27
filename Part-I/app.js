a9b492e80b0c65664164e267b2982bedc80b23e8
Sharvesh83 <sharvesh.somasundaram@kalvium.community>
2023-07-27 16:41:15 +0530

    Iteration 3 Done

--- Part-I/app.js
+++ Part-I/app.js
@@ -1,2 +1,7 @@
+const Container = () =>{
+    return React.createElement(`div`,null,`Hey Kalvians! Welcome to React Learning`,
+       React.createElement(`div`,null,`Let's rock and roll`)
+       );
+}
 const container = document.getElementById('react-container');
-ReactDOM.render("Hello! Welcome to React",container);
+ReactDOM.render(React.createElement(Container),container);
